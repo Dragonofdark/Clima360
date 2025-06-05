2e4f5f22f321504377884f0ab7566c16bb6c0417
Dragonofdark <lagendarygamer88@gmail.com>
2025-06-05 19:29:59 -0300

    mudansa para subir o projeto no pythonAnywhere

--- setup/settings.py
+++ setup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['DragonofDark.pythonanywhere.com']
 
 
 # Application definition
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statics')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
