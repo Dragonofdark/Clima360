9e999880fd4c2ae9218da52a4091c4cfe885a552
Dragonofdark <lagendarygamer88@gmail.com>
2025-06-05 20:14:41 -0300

    Mudanças Finais para o pythonAnywhere

--- setup/settings.py
+++ setup/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statics')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_production')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
