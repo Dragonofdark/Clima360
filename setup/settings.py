5d6b17cf0d880f9896dda8f8597b684b00f30cca
Dragonofdark <lagendarygamer88@gmail.com>
2025-06-05 18:44:50 -0300

    verificando se a secret key está chegando

--- setup/settings.py
+++ setup/settings.py
@@ -24,6 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+print("SECRET_KEY is:", os.getenv('SECRET_KEY'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
